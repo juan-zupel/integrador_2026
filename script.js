48ce58d90649c4f7ffa14394c2f5dde54975bd1f
juan_zupel <juanzupel21@gmail.com>
2026-06-29 11:34:21 -0300

    feat: creación de funcion encargada de desplegar/replegar el espacio publicitario

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+const btnDesplegarReplegarAnuncio = document.getElementById("btn_desplegar-replegar_anuncio");
+const publicidadIndividual = document.querySelectorAll(".publicidad-individual");
+const espacioPublicidad = document.querySelector(".espacio-publicidad");
+btnDesplegarReplegarAnuncio.addEventListener("click", DesplegarReplegarAnuncio);
 
+function DesplegarReplegarAnuncio() {
+    if (btnDesplegarReplegarAnuncio.textContent === "⬅️") {
+        btnDesplegarReplegarAnuncio.textContent = "➡️";
+        espacioPublicidad.style.width = "50px";
+        publicidadIndividual.forEach(anuncio => {
+            anuncio.style.display = "none";
+            anuncio.style.userSelect = "none";
+        })
+    }else {
+        btnDesplegarReplegarAnuncio.textContent = "⬅️";
+        espacioPublicidad.style.width = "100%";
+        publicidadIndividual.forEach(anuncio => {
+            anuncio.style.display = "block";
+            // anuncio.style.userSelect = "auto";
+        })
+    }
+    };
